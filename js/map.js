1e2550061812654c240d5f0b055a0d58966319ae
Konstantin Grebenev <3304040@gmail.com>
2018-06-15 19:17:38 +0300

    Добавляет выгрузку карточек

--- js/map.js
+++ js/map.js
@@ -39,25 +39,26 @@ var photosRealty = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
-
+//  Функция случайных чисел, от минимального до максимального /min, max/ или от длинны массива /array/
 var getRandom = function (min, max, array) {
   return (min || max) ? Math.floor(Math.random() * (max - min + 1)) + min : Math.floor(Math.random() * array.length);
 };
 
-var Massive = [];
 
+// Функция создания массива объектов, принимает на вход число, отдает массив объектов
 var createItems = function (numbers) {
 
+  var massive = []; // создает массив для объектов
 
-  for (var i = 0; i < numbers; i++) {
-    var obj = {};
-    obj.author = {};
+  for (var i = 0; i < numbers; i++) { // запускает цикл от количества на входе функции
+    var obj = {}; // создает основной объект
+    obj.author = {}; // создает подобъект author
     obj.offer = {};
     obj.location = {};
 
     obj.author.avatar = 'img/avatars/user0' + (i + 1) + '.png';
 
-    obj.location.x = getRandom(300, 900, 0);
+    obj.location.x = getRandom(300, 900, 0); // присваивает ключу x случайное значение от 300 до 900
     obj.location.y = getRandom(130, 630, 0);
 
     obj.offer.title = titleRelty[i];
@@ -72,52 +73,64 @@ var createItems = function (numbers) {
     obj.offer.description = '';
     obj.offer.photos = photosRealty[getRandom(0, 0, photosRealty)];
 
-
-    Massive.push(obj);
+    massive.push(obj); // вставляет объекты в массив
   }
 
-  return Massive;
+  return massive;
 };
 
+var mainMassive = createItems(8); // Присваиваем результат функции /создание 8-ми объектов в массиве/
 
-document.querySelector('.map').classList.remove('map--faded');
+var map = document.querySelector('.map').classList.remove('map--faded'); // ищет map и убирает класс map--faded
 
-var pinsTo = document.querySelector('.map__pins'); // найденый div куда вставлять pins
-// var cardBefore = document.querySelector('.map__filters-container'); // найденый перед div вставлять карточку
+var pinsTo = document.querySelector('.map__pins'); // ищет div куда вставлять пины на карте
 
-var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
-// var cardTemplate = document.querySelector('template').content.querySelector('.map__card');
+var template = document.querySelector('template');
+var pinTemplate = template.content.querySelector('.map__pin');
+var cardTemplate = template.content.querySelector('.map__card');
 
-var renderPin = function (massive) {
-  var pinElement = pinTemplate.cloneNode(true);
-  pinElement.style.left = massive.location.x - 40 / 2 + 'px';
+// Функция создания пинов /на входе массив объектов
+var createPin = function (massive) {
+  var pinElement = pinTemplate.cloneNode(true); // клонирует и присваивает переменной контент из шаблона
+  pinElement.style.left = massive.location.x - 40 / 2 + 'px'; // изменяет положение
   pinElement.style.top = massive.location.y - 40 + 'px';
-  pinElement.querySelector('img').src = massive.author.avatar;
+  pinElement.querySelector('img').src = massive.author.avatar; // ищет в pinElement тег img и в его объекте src переопределяет картинку в пине
   pinElement.querySelector('img').alt = massive.offer.title;
 
   return pinElement;
 };
 
-// var renderCard = function (massive) {
-//   var cardElement = cardTemplate.cloneNode(true);
-//   cardElement.querySelector('.popup__title').textContent = massive.offer.title;
-//   cardElement.querySelector('.popup__text--address').textContent = massive.offer.address;
-//   cardElement.querySelector('.popup__text--price').textContent = massive.offer.price;
-//   cardElement.querySelector('.popup__type').textContent = massive.offer.type;
-//
-//   return cardElement;
-// }
+// Функция создания карточки товара
+var createCard = function (massive) {
+  var cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = massive.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = massive.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = massive.offer.price;
+  cardElement.querySelector('.popup__type').textContent = massive.offer.type;
 
+  return cardElement;
+};
 
-function insertPin() {
+// Функция вставки пинов на карту / на входе массив объектов
+var insertPin = function (massive) {
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < Massive.length; i++) {
-    fragment.appendChild(renderPin(Massive[i]));
+  for (var i = 0; i < massive.length; i++) {
+    fragment.appendChild(createPin(massive[i]));
   }
   pinsTo.appendChild(fragment);
-}
+};
+
+var cardBefore = document.querySelector('.map__filters-container'); // перед ним будет вставка каточек
+
+// Функция вставки карточек
+var insertCard = function (massive) {
+  var fragment = document.createDocumentFragment();
+  fragment.appendChild(createCard(massive[0]));
+  // cardBefore.appendChild(fragment);
+  map.insertBefore(fragment, cardBefore); // ошибка
+};
 
 
-createItems(8);
-insertPin();
+insertPin(mainMassive);
+insertCard(mainMassive);
