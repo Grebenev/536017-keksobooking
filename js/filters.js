cf93ca1ad4ec11759a4da40ff96252bf9cf25ff1
Konstantin <3304040@gmail.com>
2018-07-18 20:13:45 +0300

    Изменения

--- js/filters.js
+++ js/filters.js
@@ -7,6 +7,7 @@
   var price = filterForm.querySelector('#housing-price');
   var room = filterForm.querySelector('#housing-rooms');
   var guest = filterForm.querySelector('#housing-guests');
+  var wifi = filterForm.querySelector('#filter-wifi');
 
   // ТИП
   var checkType = function (offerObject, value) {
@@ -37,15 +38,33 @@
   };
 
   // ГОСТИ
+  //
   var checkGuest = function (offerObject, value) {
-    return value === 'any' ? true : offerObject.offer.guests === +value;
+    return value === 'any' ? true : offerObject.offer.guests === +value; // возвращает TRUE или FALSE
+  };
+
+  // ЧЕКБОКСЫ
+  var checkFeatures = function (offerObject, value) {
+
+    var checked = filterForm.querySelectorAll('input[type ="checkbox"]:checked'); // Ищем все чекнутые чекбоксы
+
+    checked.forEach(function (element) { // листаем массив чекбоксов
+
+      console.log('Включен ' + element.value); // покажем значение включенного
+
+    });
+
+    // console.log('from ' + offerObject.offer.title + offerObject.offer.features.indexOf(value));
+
+    return offerObject.offer.features.indexOf(value) + 1;
+
   };
 
   // ОСНОВНАЯ
   filterForm.addEventListener('change', function () {
 
     var filterResult = window.items.filter(function (item) {
-      return checkType(item, type.value) && checkPrice(item, price.value) && checkRoom(item, room.value) && checkGuest(item, guest.value);
+      return checkType(item, type.value) && checkPrice(item, price.value) && checkRoom(item, room.value) && checkGuest(item, guest.value) && checkFeatures(item, 'wifi');
     });
 
     console.log('ТИП ' + type.value + ' ПРАЙС ' + price.value + ' КОМНАТЫ ' + room.value + ' ГОСТИ ' + guest.value);
